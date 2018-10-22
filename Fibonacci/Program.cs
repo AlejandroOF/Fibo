2e3921d27e598a88933f4db6f8a432dc215d5ba2
Alejandro Ortiz <alejandro.ortiz@unosquare.com>
2018-10-22 17:04:27 -0500

    first

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -6,7 +6,24 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
+           
+            int b = 1;
+            int c = 0;
+            
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Ingrese un numero");
+            int a = Int32.Parse(Console.ReadLine());
+            
+
+            for (int i= 1; i <= a; i++)
+            {
+               int d = c;
+                c = b;
+                b = d + c;
+                Console.WriteLine(b);
+            }
+
+            Console.ReadKey();
         }
     }
 }
